8b287354ad02fb0f87211dabda00a9dc7ce7215e
JIHADI <theakash@live.com>
2016-12-22 23:43:11 +0530

    UPDATE

--- calculator value.java
+++ calculator value.java
@@ -1,6 +1,6 @@
 package inexactUnitCalculator;
 
-import java.text.DecimalFormat;
+import java.text.DecimalFormat; 
 import java.text.NumberFormat;
                                                         
 /**
