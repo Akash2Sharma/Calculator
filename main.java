36c534d61882da3a1bebd007a196eb73e6601849
JIHADI <theakash@live.com>
2016-12-31 23:46:37 +0530

    UPDATE

--- main.java
+++ main.java
@@ -24,7 +24,7 @@ public class InexactUnitCalculatorMain {
     
     public static void main(String[] args) {
     	// Schedule this job for the event dispatch thread: creating and showing this application's GUI.
-    	SwingUtilities.invokeLater(new Runnable() { public void run() { new InexactUnitCalculator(); } });
+    	SwingUtilities .invokeLater(new Runnable() { public void run() { new InexactUnitCalculator(); } });
     }
     
     /** 
